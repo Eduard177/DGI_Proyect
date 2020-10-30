85a988bd025e5553f3561be21201559aa83533ee
Eduard <eduarro2001@gmail.com>
2020-10-30 00:25:10 -0400

    [Add]: DTO added

--- src/modules/RNC/RNC.controller.ts
+++ src/modules/RNC/RNC.controller.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { RncDTO } from './dto/rnc.dto';
 import { RncService } from './rnc.service';
 
 @Controller('scraping')
 export class RncController {
     constructor(private readonly rncService: RncService) {}
     @Get('rnc')
-    async test(){
-        return this.rncService.crawl('https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/rnc.aspx#','130627267');
+    async rnc(@Body() rncDto: RncDTO){
+        return this.rncService.crawl('https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/rnc.aspx#',rncDto.RNC);
     }
 }
---
+++ src/modules/RNC/dto/rnc.dto.ts
@@ -0,0 +1,3 @@
+export class RncDTO{
+    RNC: string;
+}
