803a298ce35e36b26c90412eae61df49d1f0d648
JeanSantana <jperez@xpertcode.com.do>
2020-11-23 16:36:46 -0400

    develop  : - [fix] ncf & rnc service refactoring - [add] ncf & rnc interface - [remove] unnecessary DTO'S

--- src/modules/ncf/ncf.service.ts
+++ src/modules/ncf/ncf.service.ts
@@ -2,39 +2,57 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectPage } from 'nest-puppeteer';
 import type { Page } from 'puppeteer';
+import { NcfFetchedDataInterface } from './interfaces/ncfFetchedData.interface';
 
 @Injectable()
 export class NcfService {
-    constructor(@InjectPage() private readonly page: Page) {}  
+    constructor(@InjectPage() private readonly page: Page) {}
 
-    async crawl(url: string, RNC: string, NFC: string){
+    async fetchRncDataByWebScrapping(url: string, rnc: string, ncf: string) : Promise<NcfFetchedDataInterface> {
+        await this.crawl(url);
+        await this.foundValue(rnc, ncf);
+        return await this.isValue();
+    }
+
+    async crawl(url: string){
         await this.page.goto(url, { waitUntil: 'networkidle2' });
         await this.page.content();
-        await this.foundValue(RNC, NFC);
     };
         
-    async foundValue(RNC: string, NCF: string){
-        await this.page.type("#cphMain_txtRNC",RNC);
-        await this.page.type("#cphMain_txtNCF",NCF);
+    async foundValue(rnc: string, ncf: string){
+        await this.page.type("#cphMain_txtRNC",rnc);
+        await this.page.type("#cphMain_txtNCF",ncf);
         await this.page.click('#cphMain_btnConsultar');
         await this.page.waitFor(3000);
         await this.isValue();
     }
 
-    async isValue(){
+    async isValue() : Promise<NcfFetchedDataInterface>  {
         const isValue = await this.page.evaluate(()=>{
-            const NCF = document.querySelector('#cphMain_pResultado')
-            return NCF
+            return document.querySelector('#cphMain_pResultado')
         })
+
         if(!isValue){
-            throw new NotFoundException('Not found NCF')
-        }else{
-            const data = await this.page.evaluate(() => {
-                const tds = Array.from(document.querySelectorAll('table tbody tr td span'))
-                  return tds.map(td => td.innerHTML)
-              });
-              console.log(data);
+            throw new NotFoundException('Not found RNC')
         }
+
+        const ncfData = await this.page.evaluate(() =>  Array.from(
+          document.querySelectorAll('table tbody tr td span'))
+          .map(td => td.innerHTML)
+        );
+        return this.nfcDataFiltering(ncfData);
+    }
+
+    nfcDataFiltering(payload :Array<string>) : NcfFetchedDataInterface{
+        return {
+            rnc: payload[0],
+            socialR: payload[1],
+            ncfType: payload[2],
+            ncf: payload[3],
+            status: payload[4],
+            validTime: payload[5],
+        }
+
     }
     
 }
--- src/modules/rnc/rnc.service.ts
+++ src/modules/rnc/rnc.service.ts
@@ -1,38 +1,52 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectPage } from 'nest-puppeteer';
-import type { Page } from 'puppeteer';
+import  { Page}  from 'puppeteer';
+import { RncFetchedDataInterface } from './interfaces/rncFetchedData.interface';
 
 @Injectable()
 export class RncService {
     constructor(@InjectPage() private readonly page: Page) {}
 
-    async crawl(url: string, RNC: string){
-        await this.page.goto(url, { waitUntil: 'networkidle2' });
-        await this.page.content();
-        await this.foundValue(RNC)
-    };
-    
-    async foundValue(RNC: string){
-        await this.page.type("#ctl00_cphMain_txtRNCCedula",RNC)
-        await this.page.click('#ctl00_cphMain_btnBuscarPorRNC')
-        await this.page.waitFor(3000)
-        await this.isValue()
-    }
-    async isValue(){
-        const isValue = await this.page.evaluate(()=>{
-            const RNC = document.querySelector('#ctl00_cphMain_dvDatosContribuyentes > tbody')
-            return RNC
-        })
-        if(!isValue){
-            throw new NotFoundException('Not found RNC')
-        }else{
-            const data = await this.page.evaluate(() => {
-                const tds = Array.from(document.querySelectorAll('table tbody tr td'))
-                  return tds.map(td => td.innerHTML)
-              });
-              console.log(data);
-        }
+    async fetchRncDataByWebScrapping(url: string, rnc: string) : Promise<RncFetchedDataInterface> {
+        await this.crawl(url);
+        await this.foundValue(rnc);
+        return await this.isValue();
     }
 
+    async crawl(url: string) : Promise<void>{
+        await this.page.goto(url, { waitUntil: 'networkidle2' });
+        await this.page.content();
+    };
+    
+    async foundValue(rnc : string) : Promise<void> {
+        await this.page.type("#ctl00_cphMain_txtRNCCedula",rnc )
+        await this.page.click('#ctl00_cphMain_btnBuscarPorRNC')
+        await this.page.waitFor(3000)
+    }
+
+    async isValue() : Promise<RncFetchedDataInterface>  {
+        const isValue = await this.page.evaluate(()=>{
+            return document.querySelector('#ctl00_cphMain_dvDatosContribuyentes > tbody')
+        })
+
+        if(!isValue){
+            throw new NotFoundException('Not found RNC')
+        }
+
+        const rncData = await this.page.evaluate(() =>  Array.from(
+              document.querySelectorAll('table tbody tr td'))
+          .map(td => td.innerHTML)
+        );
+        return this.rncDataFiltering(rncData);
+    }
+
+     rncDataFiltering (payload: Array<string>) : RncFetchedDataInterface  {
+      return  {
+           businessName: payload[5],
+           economyActivity: payload[13],
+           rnc: payload[1],
+           socialR: payload[3],
+           status: payload[11]
+         }
+    }
 }
